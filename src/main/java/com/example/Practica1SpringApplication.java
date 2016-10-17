8c664b7df84c5882d776c5d9a1a877b5cde1961f
Javi <javierpuiggener@gmail.com>
2016-10-17 20:15:05 +0200

    Initial commit

---
+++ src/main/java/com/example/Practica1SpringApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica1SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica1SpringApplication.class, args);
+	}
+}
