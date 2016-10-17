783ed1792974861ac41805debfdff79bc70f54de
Javi <javierpuiggener@gmail.com>
2016-10-17 20:39:50 +0200

    Domain Jugador Created

--- src/main/java/com/example/Practica1SpringApplication.java
+++ src/main/java/com/example/Practica1SpringApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Practica1SpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Practica1SpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Practica1SpringApplication.class, args);
+    }
 }
